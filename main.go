416c69be1b6ec25e0823c38023d7fb98b53b4f8d
sandylx <sandy@nodereal.io>
2022-11-22 17:25:50 +0800

    update debug util

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/hex"
 	"log"
 
@@ -14,6 +15,9 @@ import (
 func main() {
 	// check init code hash, if deployment does not work properly
 	client, _ := ethclient.Dial("http://172.22.41.197:8545")
+	chainId, _ := client.ChainID(context.Background())
+	networkId, _ := client.NetworkID(context.Background())
+	log.Println("chainId:", chainId, "networkId:", networkId)
 	v2factory, _ := V2factory.NewV2factory(common.HexToAddress("0xBb4991862A738837C131703dA2F29f7F3075A231"), client)
 	initHash, _ := v2factory.GetPairInitHash(&bind.CallOpts{})
 	log.Println(hex.EncodeToString(initHash[:]))
